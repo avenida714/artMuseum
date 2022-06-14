8ddac16a2a101ff2f62862925717a5baa20afd90
Alec Venida <alecvenida@gmail.com>
2022-06-14 15:33:49 -0700

    1-7 done

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter} from 'react-router-dom' // everthing from outside libs first, then local
 import './index.css';
 import App from './App';
 
+
+const Root = () => {
+  return (
+    <BrowserRouter>
+      <App/>
+    </BrowserRouter>
+  );
+}
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Root />
   </React.StrictMode>,
   document.getElementById('root')
 );
